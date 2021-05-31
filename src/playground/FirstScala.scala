d0f57fc1d252ed9c062399d28d3703761b8fdca9
sajag16642 <sajag16642@gmail.com>
2021-05-31 16:20:15 +0530

    SetUp

--- src/playground/FirstScala.scala
+++ src/playground/FirstScala.scala
@@ -6,10 +6,13 @@ import scala.annotation.tailrec
 object FirstScala extends App {
   println("Hello Scala")
 
+  /*
+  Addition
+   */
   def sum(a: Int, b: Int): Int = a + b
 
-  /**
-   * Factorial program
+  /*
+  Recursion
    */
   @tailrec
   def factorial(n: Int, acc: BigInt = 1): BigInt = {
@@ -19,6 +22,31 @@ object FirstScala extends App {
       factorial(n - 1, n * acc)
   }
 
+  /*
+  Repeating string
+   */
+  @tailrec
+  def repeatedFunction(s: String, n: Int, ans: String): String =
+    if (n <= 1) ans
+    else repeatedFunction(s, n - 1, s + ans)
+
+  /*
+  Fibonacci Number
+   */
+  @tailrec
+  def fibonacci(n: Int, a: Int, b: Int): Int =
+    if (n <= 2) b
+    else fibonacci(n - 1, b, a + b)
+
+  /*
+  Prime Number
+   */
+  def isPrime(n: Int): Boolean =
+    true
+
+
   println(sum(2, 3))
   println(factorial(5000))
+  println(repeatedFunction("Hello", 3, "Hello"))
+  println(fibonacci(3, 1, 1))
 }
