3c8f7d9c230c0d78d804359c053541f910289247
sajag16642 <sajag16642@gmail.com>
2021-06-08 10:27:36 +0530

    generics

--- src/Exercises/MyList.scala
+++ src/Exercises/MyList.scala
@@ -4,33 +4,51 @@ package Exercises
 /**
  * @author sajag16642
  */
-abstract class MyList {
-  def head: Int
+abstract class MyList[+A] {
+  def head: A
 
-  def tail: MyList
+  def tail: MyList[A]
 
   def isEmpty: Boolean
 
-  def add(e: Int): MyList
+  def add[B >: A](e: B): MyList[B]
+
+  def printElements: String
+
+  override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
 
-  override def tail: MyList = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
 
-  override def isEmpty: Boolean = true
+  def isEmpty: Boolean = true
 
-  override def add(e: Int): MyList = new Cons(e, Empty)
+  def add[B >: Nothing](e: B): MyList[B] = new Cons(e, Empty)
+
+  def printElements: String = ""
 }
 
 
-class Cons(head: Int, tail: MyList) extends MyList {
-  override def head: Int = head
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head:A = h
 
-  override def tail: MyList = tail
+  def tail:MyList[A] = t
 
-  override def isEmpty: Boolean = false
+  def isEmpty: Boolean = false
 
-  override def add(e: Int): MyList = new Cons(e, this)
+  def add[B >: A](e: B): MyList[B] = new Cons(e, this)
+
+  def printElements: String = {
+    if (t.isEmpty) "" + h
+    else h + " " + t.printElements
+  }
+}
+
+object Test extends App {
+  val listOfIntegers: MyList[Int] = new Cons(1,new Cons(2,new Cons(3,Empty)))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("World",Empty))
+  println(listOfStrings)
+  println(listOfIntegers)
 }
