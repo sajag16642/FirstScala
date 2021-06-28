7e1691ad1d44d823beb25292903e97536c70c95e
sajag16642 <sajag16642@gmail.com>
2021-06-28 12:22:13 +0530

    social network better

--- src/Exercises/SocialNetwork.scala
+++ src/Exercises/SocialNetwork.scala
@@ -1,6 +1,8 @@
 package org.scala.practice
 package Exercises
 
+import sun.rmi.transport.Target
+
 import scala.annotation.tailrec
 
 /**
@@ -11,13 +13,14 @@ object SocialNetwork extends App {
     network + (person -> Set())
   }
 
-  def remove(network: Map[String,Set[String]], person: String): Map[String,Set[String]]={
-      @tailrec
-      def unfriendEach(remFriends: Set[String], remNetwork: Map[String,Set[String]]): Map[String,Set[String]] = {
-        if(remFriends.isEmpty) remNetwork
-        else unfriendEach(remFriends.tail,unfriend(remNetwork,person,remFriends.head))
-      }
-    unfriendEach(network(person),network) - person
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    @tailrec
+    def unfriendEach(remFriends: Set[String], remNetwork: Map[String, Set[String]]): Map[String, Set[String]] = {
+      if (remFriends.isEmpty) remNetwork
+      else unfriendEach(remFriends.tail, unfriend(remNetwork, person, remFriends.head))
+    }
+
+    unfriendEach(network(person), network) - person
   }
 
   def friend(network: Map[String, Set[String]], A: String, B: String): Map[String, Set[String]] = {
@@ -32,11 +35,37 @@ object SocialNetwork extends App {
     network + (A -> (friendsA - B)) + (B -> (friendsB - A))
   }
 
-  val network: Map[String, Set[String]] = add(add(Map(),"Bob"), "Alice")
+  def nFriend(network: Map[String, Set[String]], person:String):Int = {
+    if(!network.contains(person)) 0
+    else network(person).size
+  }
+
+  def mostFriends(network: Map[String, Set[String]]): String = {
+    network.maxBy(pair => pair._2.size)._1
+  }
+
+  def nPeopleWithNoFriends(network: Map[String, Set[String]]): Int = {
+    network.count(pair=>pair._2.isEmpty)
+  }
+
+  def socialConnection(network: Map[String, Set[String]], a: String, b: String): Boolean = {
+    def bfs(target: String, consideredPeople: Set[String], discoverablePeople: Set[String]): Boolean = {
+      if(discoverablePeople.isEmpty) false
+      else{
+        val person = discoverablePeople.head
+        if(person == target) true
+        else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoverablePeople.tail)
+        else bfs(target, consideredPeople+person, discoverablePeople.tail ++ network(person))
+      }
+    }
+    bfs(b,Set(), network(a)+a)
+  }
+
+  val network: Map[String, Set[String]] = add(add(Map(), "Bob"), "Alice")
   println(network)
-  println(friend(network,"Bob","Alice"))
-  println(unfriend(friend(network,"Bob","Alice"),"Bob","Alice"))
-  println(remove(friend(network,"Bob","Alice"),"Bob"))
+  println(friend(network, "Bob", "Alice"))
+  println(unfriend(friend(network, "Bob", "Alice"), "Bob", "Alice"))
+  println(remove(friend(network, "Bob", "Alice"), "Bob"))
 
 }
 
